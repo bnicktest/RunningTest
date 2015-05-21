2cce866d063e692d5f6a6c17ba83ffa03b930867
bn1ck <nbalatsky@gmail.com>
2015-05-21 00:46:45 -0700

    ViewModels
    - Added basic login functionality

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
@@ -10,7 +11,18 @@ namespace SimpleBlog.Controllers
     {
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin
+            {
+            });
+        }
+
+        [HttpPost] //only when post request is sent method is initiated
+        public ActionResult Login(AuthLogin form) //parameters come from authlogin viewmodel. 
+        {
+            if(!ModelState.IsValid)
+                return View(form);
+
+            return Content("The form is valid");
         }
     }
 }
