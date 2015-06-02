ecc84f77a8d4a38f36e08ecfa46b38d2fe7608e9
bn1ck <nbalatsky@gmail.com>
2015-06-01 23:24:09 -0700

    Database Auth
    Created database authorization
    Hashed passwords in DB

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using System.Web.Security;
+using NHibernate.Linq;
+using SimpleBlog.Models;
 using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
@@ -17,18 +16,22 @@ namespace SimpleBlog.Controllers
         }
         public ActionResult Login()
         {
-            return View(new AuthLogin
-            {
-            });
+            return View(new AuthLogin());
         }
 
         [HttpPost] //only when post request is sent method is initiated
         public ActionResult Login(AuthLogin form, string returnUrl) //parameters come from authlogin viewmodel. 
         {
-            if(!ModelState.IsValid)
+            var user = Database.Session.Query<User>().FirstOrDefault(u => u.Username == form.Username);
+            if(user == null)
+                Models.User.FakeHash();
+            if(user == null || !user.CheckPassword(form.Password))
+                ModelState.AddModelError("Username","Username or password is incorrect");
+
+            if (!ModelState.IsValid)
                 return View(form);
 
-            FormsAuthentication.SetAuthCookie(form.Username, true);
+            FormsAuthentication.SetAuthCookie(user.Username, true);
 
             if(!string.IsNullOrWhiteSpace(returnUrl))
                 return Redirect(returnUrl);
