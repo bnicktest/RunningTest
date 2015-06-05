085adab84fc782de71de0646722c87c998d08074
bn1ck <nbalatsky@gmail.com>
2015-06-05 15:54:32 -0700

    Pagination For posts admin
    Created pages in the post admin
    Created edit and delete button

---
+++ Areas/Admin/ViewModels/Posts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using SimpleBlog.Infrastructure;
+using SimpleBlog.Models;
+
+namespace SimpleBlog.Areas.Admin.ViewModels
+{
+    public class PostsIndex
+    {
+        public PageData<Post> Posts { get; set; }
+    }
+}
---
+++ Migrations/_003_AddContentsToPosts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentMigrator;
+
+namespace SimpleBlog.Migrations
+{
+    [Migration(3)]
+    public class _003_AddContentsToPosts : Migration
+    {
+        public override void Up()
+        {
+            Create.Column("content").OnTable("posts").AsCustom("TEXT");
+        }
+
+        public override void Down()
+        {
+            Delete.Column("content").FromTable("posts");
+        }
+    }
+}
